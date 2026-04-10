aefadbc1735cca79a1681cc757afdb9675215495
miobi3698 <44633176+miobi3698@users.noreply.github.com>
2026-04-10 20:29:49 +0700

    Fix Lua OOP patterns and code quality issues in starfield simulation
    - Declare Width/Height/Star as local to avoid global leaks
    - Fix metatable inheritance pattern in Star:new()
    - Use instance variable 'o' instead of class 'self' for initialization
    - Replace love.graphics.clear() with setBackgroundColor()
    - Extract magic numbers (STAR_SPEED, STAR_COUNT, STAR_COLOR) as constants
    - Add .gitignore

--- main.lua
+++ main.lua
@@ -1,18 +1,19 @@
----@param n number
----@param start1 number
----@param stop1 number
----@param start2 number
----@param stop2 number
----@return number
 local function map(n, start1, stop1, start2, stop2)
   return (n - start1) / (stop1 - start1) * (stop2 - start2) + start2
 end
+
+local STAR_SPEED = 500
+local STAR_COUNT = 200
+local STAR_COLOR = { 1, 1, 1 }
+
+local Width, Height
 
 ---@class Star
 ---@field x number
 ---@field y number
 ---@field z number
-Star = {
+---@field pz number
+local Star = {
   x = 0,
   y = 0,
   z = 0,
@@ -22,18 +23,17 @@ Star = {
 ---@return Star
 function Star:new(o)
   o = o or {}
-  setmetatable(o, self)
-  self.__index = self
-  self.x = love.math.random(-Width, Width)
-  self.y = love.math.random(-Height, Height)
-  self.z = love.math.random(Width)
-  self.pz = self.z
+  setmetatable(o, { __index = self })
+  o.x = love.math.random(-Width, Width)
+  o.y = love.math.random(-Height, Height)
+  o.z = love.math.random(Width)
+  o.pz = o.z
   return o
 end
 
 ---@param dt number
 function Star:update(dt)
-  self.z = self.z - (500 * dt)
+  self.z = self.z - (STAR_SPEED * dt)
   if self.z < 1 then
     self.x = love.math.random(-Width, Width)
     self.y = love.math.random(-Height, Height)
@@ -46,6 +46,7 @@ function Star:show()
   local sx = map(self.x / self.z, -1, 1, -Width / 2, Width / 2) + Width / 2
   local sy = map(self.y / self.z, -1, 1, -Height / 2, Height / 2) + Height / 2
   local r = map(self.z, 0, Width, 4, 0)
+  love.graphics.setColor(STAR_COLOR)
   love.graphics.circle("fill", sx, sy, r)
 
   local px = map(self.x / self.pz, -1, 1, -Width / 2, Width / 2) + Width / 2
@@ -57,24 +58,23 @@ end
 
 ---@type Star[]
 local stars = {}
-local star_count = 200
 
 function love.load()
   Width, Height = love.graphics.getDimensions()
-  for i = 1, star_count do
+  love.graphics.setBackgroundColor(0, 0, 0)
+  for i = 1, STAR_COUNT do
     stars[i] = Star:new()
   end
 end
 
 function love.update(dt)
-  for i = 1, star_count do
+  for i = 1, STAR_COUNT do
     stars[i]:update(dt)
   end
 end
 
 function love.draw()
-  love.graphics.clear(0, 0, 0, 255)
-  for i = 1, star_count do
+  for i = 1, STAR_COUNT do
     stars[i]:show()
   end
 end
